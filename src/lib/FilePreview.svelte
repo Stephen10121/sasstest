56d03f707a4d9d275144ab20526b69d69879d68d
Stephen Gruzin <stephengrzn@gmail.com>
2025-05-08 14:55:17 -0700

    Changed the file preview based on the mime type of the file

--- src/lib/FilePreview.svelte
+++ src/lib/FilePreview.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
-    import { DownloadIcon, Mouse } from "lucide-svelte";
+    import { DownloadIcon } from "lucide-svelte";
+    import mime from "mime";
+    import { Button } from "./components/ui/button";
     const { filePath, fileName, close, download }: { filePath: string, fileName: string, close: () => unknown, download: (fileName: string) => unknown } = $props();
 
     let filePathParsed = filePath.split("/").slice(2).join("/");
+    let fileMimeType = mime.getType(fileName)?.split("/")[0];
 
     function mouseDown(e: MouseEvent) {
         //@ts-ignore
@@ -13,7 +16,6 @@
 </script>
 
 <section class="dark:bg-white/20 bg-black/50" onmousedown={mouseDown} role="none">
-    <!-- <div class="dark:bg-white/50 bg-black/50 controls"> -->
     <div class="controls" id="filePreviewBackdropControls">
         <div class="dark:bg-black bg-white innerControls">
             <button title="Download File" onclick={() => download(fileName)}><DownloadIcon /></button>
@@ -21,15 +23,25 @@
         <button class="dark:bg-black bg-white closeButton" title="Close Preview" onclick={() => close()}>✕</button>
     </div>
     <div class="preview" id="filePreviewBackdropPreview">
-        <video id="videoPlayerCool" controls autoplay controlsList="nodownload">
-            <source src="/api/fileStream?fileName={encodeURIComponent(fileName)}&filePath={encodeURIComponent(filePathParsed)}">
-            <track kind="captions">
-            <a
-                href="https://archive.org/download/ElephantsDream/ed_1024_512kb.mp4"
-                download="ed_1024_512kb.mp4">
-                Download
-            </a>
-        </video>
+        {#if fileMimeType === "video"}
+            <video id="videoPlayerCool" controls autoplay controlsList="nodownload">
+                <source src="/api/fileStream?fileName={encodeURIComponent(fileName)}&filePath={encodeURIComponent(filePathParsed)}" />
+                <track kind="captions">
+                Your browser does not support the audio tag.
+                <button onclick={() => download(fileName)}>Download File Instead</button>
+            </video>
+        {:else if fileMimeType === "audio"}
+            <audio controls autoplay controlsList="nodownload">
+                <source src="/api/fileStream?fileName={encodeURIComponent(fileName)}&filePath={encodeURIComponent(filePathParsed)}" />
+                Your browser does not support the audio tag.
+                <button onclick={() => download(fileName)}>Download File Instead</button>
+            </audio>
+        {:else if fileMimeType === "image"}
+            <img src="/api/fileDownload?fileName={encodeURIComponent(fileName)}&filePath={encodeURIComponent(filePathParsed)}" alt="File Preview of {fileName}">
+        {:else}
+            <p class="text-white text-lg">Sorry. This file cannot be previewed.</p>
+            <Button onclick={() => download(fileName)} class="text-xs">Download File Instead</Button>
+        {/if}
     </div>
 </section>
 
@@ -84,12 +96,18 @@
         display: flex;
         align-items: center;
         justify-content: center;
+        flex-direction: column;
+        gap: 20px;
         width: 100%;
         height: 100%;
     }
 
     video {
         max-width: 1000px;
-        width: 100%;
+        width: 100%; 
+    }
+
+    img {
+        max-width: min(1000px, 100%);
     }
 </style>
